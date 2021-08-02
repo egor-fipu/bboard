2420e37b4c7e1297f8baef05c538bdb1c1775f47
Егор Коростелев <egor.fipu@gmail.com>
2021-08-02 13:59:26 +0700

    Update ALLOWED_HOSTS

--- bboard/settings.py
+++ bboard/settings.py
@@ -17,7 +17,10 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'my-bboard.herokuapp.com',
+    '127.0.0.1',
+]
 
 # Application definition
 
